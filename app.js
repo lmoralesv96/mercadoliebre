29b12772ae0b26525fbf0acfc48b3ee1c42c7633
lmoralesv96 <lmoralesv1100@alumno.ipn.mx>
2021-08-18 00:33:34 -0500

    Se hacen configuraciones para carga a Heroku

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const publicPath = path.resolve(__dirname,'./public');
 
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
-    console.log('Servidor corriendo en 3000');
+let port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log("Servidor funcionando");
 });
 
 app.get('/', (req,res)=>{
